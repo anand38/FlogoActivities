4d91ea61365c59f80ce45d1c00e824728f14677a
anand38 <anandbrahmadev@gmail.com>
2018-05-12 01:40:10 +0530

    some changes

--- Activities/smartsheet/activity.go
+++ Activities/smartsheet/activity.go
@@ -45,31 +45,8 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 					fmt.Print("Error Occurred: ",err_resp)
 				}else {
 					sheetData,_:=ioutil.ReadAll(success_resp.Body)
-
-					/*
-					columns:=gjson.Get(string(sheetData),"columns")
-					columnLength,_:=strconv.Atoi(gjson.Get(columns.String(),"#").String())
-					var col=make([]string,columnLength)
-					for t:=0;t<columnLength;t++{
-						col[t]=gjson.Get(columns.String(),strconv.Itoa(t)+".title").String()
-					}
-					rows := gjson.Get(string(sheetData),"rows.#.cells")
-					activityOutputTmp:=`{}`
-					
-					for i,_ := range rows.Array(){
-						rowcell:= gjson.Get(rows.String(),strconv.Itoa(i)) ///single row
-						for tmp:=0;tmp<columnLength;tmp++  {
-							activityOutputTmp,_=sjson.Set(activityOutputTmp,"rows."+strconv.Itoa(i)+"."+col[tmp],gjson.Get(rowcell.String(),strconv.Itoa(tmp)+".value").String())
-						}
-					}
-					activityOutput=activityOutputTmp
-
-					*/
-					activityOutput=
-
-
+					activityOutput=smartsheetcode.SetSheetDetails(string(sheetData))
 					fmt.Println(activityOutput)
-					
 				}
 			}
 	context.SetOutput("Response_Json", activityOutput)
