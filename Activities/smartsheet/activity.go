60c8ec637a0d87af28b801a7969ff7c6fe7229fe
anand38 <anandbrahmadev@gmail.com>
2018-05-12 11:01:21 +0530

    Update activity.go

--- Activities/smartsheet/activity.go
+++ Activities/smartsheet/activity.go
@@ -2,17 +2,11 @@ package smartsheet
 
 import (
 	"github.com/TIBCOSoftware/flogo-lib/core/activity"
-	"net/http"
-	"io/ioutil"
-	"github.com/anand38/FlogoActivities/smartsheetcode"
 	"github.com/TIBCOSoftware/flogo-lib/logger"
-	"errors"
-	"github.com/tidwall/gjson"
-	"fmt"
+	"github.com/anand38/FlogoActivities/smartsheetcode"
 )
 
-// ActivityLog is the default logger for the Log Activity
-var activityLog = logger.GetLogger("activity-flogo-SmartSheet-getSheetDetails")
+
 
 // MyActivity is a stub for your Activity implementation
 type MyActivity struct {
@@ -37,38 +31,13 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error)  {
 	accessToken:=context.GetInput("Access_Token").(string)
 	sheetId:=context.GetInput("Sheet_ID").(string)
 	activityOutput:=""
-	errReturn:=""
 
-			sheetUrl:="https://api.smartsheet.com/2.0/sheets/"+sheetId
-			{
-				req,_:=http.NewRequest("GET",sheetUrl,nil)
-				req.Header.Set("Authorization","Bearer "+accessToken)
-				req.Header.Set("Content-Type","application/json")
-				cl := &http.Client{}
-				success_resp,err_resp := cl.Do(req)
-				if err_resp !=nil{
-					errReturn="The HTTP request failed while getting sheet details..."
-					//fmt.Print("Error Occurred: ",err_resp.Error())
-					activityLog.Errorf("some error occurred while trying to fetch sheet details...")
-					return false,errors.New(errReturn)
-				}else {
-					sheetData,_:=ioutil.ReadAll(success_resp.Body)
-					logger.Debug(sheetData)
-					//fmt.Println(string(sheetData))
-					errCode:=gjson.Get(string(sheetData),"errorCode")
-					if(errCode.Exists()){
-						errMessage:=gjson.Get(string(sheetData),"message").String()
-						activityLog.Errorf(errMessage)
-						fmt.Println(errMessage)
-						return false,errors.New(errMessage)
-					}
-					activityOutput=smartsheetcode.SetSheetDetails(string(sheetData))
-					//fmt.Println(activityOutput)
-					logger.Debug(activityOutput)
-				}
-			}
-
-	context.SetOutput("Response_Json", activityOutput)
+	activityOutput,err=smartsheetcode.GetSheetDetails(sheetId,accessToken)
+	if err!=nil {
+		return false,err
+	}else{
+		context.SetOutput("Response_Json", activityOutput)
+	}
 	return true, nil
 	}
 
