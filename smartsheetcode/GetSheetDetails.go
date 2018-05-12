dad0af5470e46acdf58ffe630a2d0f149443d1cb
anand38 <anandbrahmadev@gmail.com>
2018-05-12 11:00:31 +0530

    Update GetSheetDetails.go

--- smartsheetcode/GetSheetDetails.go
+++ smartsheetcode/GetSheetDetails.go
@@ -9,29 +9,6 @@ import (
 	"fmt"
 )
 
-/*
-func SetSheetDetails(sheetData string)string{
-	activityOutput:=""
-
-	columns:=gjson.Get(string(sheetData),"columns")
-	columnLength,_:=strconv.Atoi(gjson.Get(columns.String(),"#").String())
-	var col=make([]string,columnLength)
-	for t:=0;t<columnLength;t++{
-		col[t]=gjson.Get(columns.String(),strconv.Itoa(t)+".title").String()
-	}
-	rows := gjson.Get(string(sheetData),"rows.#.cells")
-	activityOutputTmp:=`{}`
-
-	for i,_ := range rows.Array(){
-		rowcell:= gjson.Get(rows.String(),strconv.Itoa(i)) ///single row
-		for tmp:=0;tmp<columnLength;tmp++  {
-			activityOutputTmp,_=sjson.Set(activityOutputTmp,"rows."+strconv.Itoa(i)+"."+col[tmp],gjson.Get(rowcell.String(),strconv.Itoa(tmp)+".value").String())
-		}
-	}
-	activityOutput=activityOutputTmp
-	return activityOutput
-}
-*/
 
 func GetSheetDetails(sheetId string,accessToken string)(string,error){
 	// ActivityLog is the default logger for the Log Activity
